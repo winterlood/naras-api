80e6f8290fc485df23b94f24bef874ec19312296
winterlood <king199777@gmail.com>
2023-07-19 15:02:05 +0900

    Revert "✨ add request validator"
    This reverts commit 6c2dee98c1cf798edfbb67a9c9a9c5294f281d0c.

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -29,8 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const countries_json_1 = __importDefault(require("./resource/countries.json"));
-const dotenv = __importStar(require("dotenv"));
-dotenv.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 const port = process.env.PORT || 8080;
@@ -48,18 +23,6 @@ function convertCountryItem(countries) {
         };
     });
 }
-// Validator
-app.use(function (req, res, next) {
-    const authorization = req.headers.authorization;
-    if (authorization === process.env.SECRET_KEY) {
-        next();
-    }
-    else {
-        return res
-            .status(403)
-            .send("Authorization 키를 확인하세요");
-    }
-});
 app.get("/", (_req, res) => {
     return res.send("THIS IS NARAS API");
 });
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import countries from "./resource/countries.json";
 import { CountryFullInfo, CountryItem } from "./types";
-import * as dotenv from "dotenv";
-dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -32,18 +30,6 @@ function convertCountryItem(countries: CountryFullInfo[]) {
     };
   });
 }
-
-// Validator
-app.use(function (req, res, next) {
-  const authorization = req.headers.authorization;
-  if (authorization === process.env.SECRET_KEY) {
-    next();
-  } else {
-    return res
-      .status(403)
-      .send("Authorization 키를 확인하세요");
-  }
-});
 
 app.get("/", (_req: Request, res: Response) => {
   return res.send("THIS IS NARAS API");
